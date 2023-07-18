bd685a29efa6647ba3d020967c71ff1dbd0fa1a8
AbbasTestAccount <abbasfake5@gmail.com>
2023-07-18 13:15:01 +0330

    init base and main Fragment

---
+++ app/src/main/java/com/example/n2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.n2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.n2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setFirstFrag()
+    }
+
+    private fun setFirstFrag() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.mainFrame, SmsListFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
